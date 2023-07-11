e4a5088ef2ad5451f24a6da868b985b759f3b207
Anand <anandpawar8861@gmail.com>
2023-07-11 21:07:18 +0530

    Fronted application join

--- src/main/java/com/anand/portfolio/controller/PortfolioController.java
+++ src/main/java/com/anand/portfolio/controller/PortfolioController.java
@@ -1,6 +1,7 @@
 package com.anand.portfolio.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import com.anand.portfolio.entities.PortfolioEntity;
 import com.anand.portfolio.service.PortfolioService;
 
 @RestController
+@CrossOrigin("*")
 public class PortfolioController {
 
 	@Autowired
